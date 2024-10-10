a43eabe73f2ba21e88813f87e5c8675820d4fbe0
ZiadAS40 <zead4036@gmail.com>
2024-10-11 01:19:40 +0300

    adding all files without the advanced ones

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes a float a and a float b.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating points numbers.
+    """
+    return a + b
